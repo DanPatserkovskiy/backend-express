86079f64d2e68555e9ce4439f9001ab9fb248bff
Пацерковский Даниил <patserkovskiy@list.ru>
2026-03-24 18:24:31 +0500

    7-8

--- express-app/app.js
+++ express-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const querystring = require('querystring');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,6 +14,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  if (req.query["auth"] !== "true") {
+    return res.sendStatus(401);
+  }
+  return next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const router = express.Router();
-items = [{id: 1, name: 'Миша'}, {id: 2, name: 'Даня'}];
+
+let id = 1;
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP)`);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(items);
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
-router.get('/:id', function(req, res, next) {
-  const {id} = req.params;
-  const user = items.find(item => item.id === +id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({message: 'Пользователь не найден'});
-  }
-});
-
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   const {name} = req.body;
-  const newUser = {id: items.length + 1, name};
-  items.push(newUser);
-  res.status(201).json(newUser);
-});
+  if (!req.body.name || !req.body.email) {
+    return res.status(400).json({
+      message: "Пользователь не обнаружен"
+    })
+  }
+  const newUser = {
+    id: id++,
+    name: req.body.name,
+    email: req.body.email,
+    createdAt: new Date()
+  };
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  db.run(insert, [name]);
+  res.status(200).json({newUser})
+})
 
 module.exports = router;
