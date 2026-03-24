2c3dc3a8269eaf94b1dd79d9e24d456d27d4ca3b
sarnatskiy <mikhailsarnatskiy@yandex.ru>
2026-03-24 17:41:31 +0500

    1-6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+items = [{id: 1, name: 'Миша'}, {id: 2, name: 'Даня'}];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(items);
+});
+
+router.get('/:id', function(req, res, next) {
+  const {id} = req.params;
+  const user = items.find(item => item.id === +id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({message: 'Пользователь не найден'});
+  }
+});
+
+router.post('/', function(req, res, next) {
+  const {name} = req.body;
+  const newUser = {id: items.length + 1, name};
+  items.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
